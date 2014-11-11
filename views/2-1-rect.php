5d6a5938e537af8a144b5309b6a483ee710403f1
lirui <liruixd@verycd.com>
2014-11-11 19:35:55 +0800

    canvas 2-1-rect

---
+++ views/2-1-rect.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>2-1-rect</title>
+        <style>
+            body{background: #ddd;}
+            #canvas{
+                background: #fff;
+                margin: 20px;
+                padding: 20px;
+                border: inset thin #aaa;
+            }
+        </style>
+    </head>
+    <body>
+        <canvas id="canvas" width="600" height="400"></canvas>
+        <script>
+            var canvas = document.getElementById('canvas');
+            var context = canvas.getContext('2d');
+            var cW = canvas.width, cH = canvas.height;
+
+            context.lineJoin = 'round';
+            context.lineWidth = 30;
+            context.font = "Arial 24px";
+            context.fillText('Click anywhere to ease', 175, 40);
+
+            context.strokeRect(75, 100, 200, 200);//矩形描边
+            context.fillRect(325, 100, 200, 200);//矩形填充
+
+            context.canvas.onmousedown = function(){
+                context.clearRect(0, 0, cW, cH);
+            }
+        </script>
+    </body>
+</html>
